26b7889c53861695ee2571f577a622d91b98e1d8
liza-hazard <31432044+liza-hazard@users.noreply.github.com>
2023-12-22 16:17:15 +0000

    carbon datong

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -18,7 +18,7 @@ const HALF_LIFE_PERIOD = 5730;
  *
  */
 function dateSample(sampleActivity) {
-  throw new NotImplementedError('Not implemented');
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
   if (typeof(sampleActivity) == 'string' && sampleActivity.trim() != '') {
     sampleActivity = +sampleActivity;
